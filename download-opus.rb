972aacbec2cb08ecf769e07120ec75a62ae51efe
Vee Satayamas <vee.sa@protonmail.com>
2020-02-01 14:52:43 +0700

    fix language pair filters and add a name filter

--- download-opus.rb
+++ download-opus.rb
@@ -5,27 +5,34 @@ $target = "th"
 $prefix = "corpus"
 corpora = JSON.parse(`curl 'http://opus.nlpl.eu/opusapi/?source=#{$source}&target=#{$target}'`)["corpora"]
 moses_names = corpora.select {|c| c["preprocessing"] == "moses"}
-                     .select {|c| c["latest"]}
-                     .map {|c| c["corpus"]}
+                .select {|c| c["latest"]}
+                .map {|c| c["corpus"]}
+
 xml_names = corpora.select {|c| c["preprocessing"] == "xml"}
-                   .select {|c| c["latest"]}
-                   .select {|c| c["source"] == $source}
-                   .select {|c| c["target"] == $target}
-                   .map {|c| c["corpus"]}
-names = moses_names & xml_names
+              .select {|c| c["latest"]}
+              .select {|c| c["source"] == $source}
+              .select {|c| c["target"] == $target}                      
+              .map {|c| c["corpus"]}
+$names = moses_names & xml_names
 
 def download_corpus(c)
   filename = c["url"].split(/\//)[-1]
   dir = File.join($prefix, c["corpus"], "latest", c["preprocessing"])
   `mkdir -p #{dir}`
   file_path = File.join(dir, filename)
-  `curl #{c["url"]} -o #{file_path}`
+  cmd = "curl #{c["url"]} -o #{file_path}"
+  puts cmd
+  `#{cmd}`
 end
 
 corpora.select {|c| c["preprocessing"] == "moses"}
-       .select {|c| c["latest"]}
-       .each {|c| download_corpus(c)}
+  .select {|c| $names.include?(c["corpus"])}
+  .select {|c| c["latest"]}
+  .each {|c| download_corpus(c)}
 
 corpora.select {|c| c["preprocessing"] == "xml"}
-       .select {|c| c["latest"]}
-       .each {|c| download_corpus(c)}
+  .select {|c| $names.include?(c["corpus"])}
+  .select {|c| c["latest"]}
+  .select {|c| c["source"] == $source}
+  .select {|c| c["target"] == $target}
+  .each {|c| download_corpus(c)}
